cf8d0d8979a2478b649edfb55cb59d3ca8fa3c68
Pedro Igor <pedroigor2110@gmail.com>
2022-09-10 00:20:31 -0300

    ajustando componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
 import NotificationButton from "./components/notification-button"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+
+
+
 
 function App() {
- return(
+  return (
     <>
-  <h1>Olá Mundo!</h1>
-  <NotificationButton></NotificationButton>
-  </>
- )
+     
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard></SalesCard>
+          
+          </div>
+
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
